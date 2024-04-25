f30bef622e60fc88371c82b2883fafadea848371
AliAbdelAall <3laych@gmail.com>
2024-04-25 13:53:47 +0300

    added routes for Auth page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./pages/Auth"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Auth/>}>
+        <Route index Element/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 

a016c569cd7b2534453eb66247c6ca733502f962
AliAbdelAall <3laych@gmail.com>
2024-04-25 18:02:20 +0300

    added localStorage custom tool

---
+++ web/src/core/tools/local/user.js
@@ -0,0 +1,12 @@
+export const setLocalUser = (token) => {
+  localStorage.token = token
+}
+
+export const getLocalUser = () => {
+  const token = localStorage.token
+  return token ? token : false
+}
+
+export const removeLocalUser = () => {
+  localStorage.removeItem("token")
+}

26c541f03b471b56e2ecaf08b0693341030afd6a
AliAbdelAall <3laych@gmail.com>
2024-04-25 17:02:13 +0300

    updated Login component in web

--- web/src/components/LoginInput/index.jsx
+++ web/src/components/LoginInput/index.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 // Styles
 import "./style.css"
 
-const LoginInput = ({id, label, placeholder}) => {
+const LoginInput = ({id, label, placeholder, handleChange}) => {
   return (
 
     <div className='flex column login-input'>
       <label className='font-medium' htmlFor={id}>{label}</label>
-      <input className='font-light' type="text" id={id} placeholder={placeholder}/>
+      <input className='font-light' type="text" id={id} placeholder={placeholder} onChange={handleChange}/>
     </div>
   )
 }
--- web/src/pages/Auth/components/Login/index.jsx
+++ web/src/pages/Auth/components/Login/index.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 
+// assets
 import fullLogo from "../../../../assets/logo/full-logo.png"
+
+// components
+import LoginInput from '../../../../components/LoginInput'
+import LoginButton from '../../../../components/LoginButton'
+
 
 const Login = () => {
   return (
     <div className='flex column align-center login-container'>
       <img src={fullLogo} width={100} height={120} alt="logo" />
 
-      <div className='flex column login-imput'>
-        <label htmlFor="username-input">Username</label>
-        <input type="text" id='username-input' placeholder='JhonDoe'/>
+      <div className='flex column input-wrapper'>
+        <LoginInput
+        id={"username-input"}
+        label={"Username"}
+        placeholder={"JhonDoe"}
+        // handleChange={}
+        />
+
+        <LoginInput
+        id={"password-input"}
+        label={"Password"}
+        placeholder={"********"}
+        // handleChange={}
+        />
+
+        <p className='text-acient font-medium'>Forgot password?</p>
       </div>
-      <div className='flex column login-imput'>
-        <label htmlFor="username-input">Username</label>
-        <input type="text" id='username-input' placeholder='JhonDoe'/>
+
+      <div className='flex column center full-width login-wrapper'>
+        <LoginButton
+        text={"Log in"}
+        // handleClick={}
+        />
+
+        <p className='text-acient'>Don't have an account? <span className='font-medium text-primary auth-switch'>Sign up</span></p>
       </div>
-
-      <p>Forgot password?</p>
-
-      <button>Log in</button>
-
-      <p>Don't have an account? <span>Sign up</span></p>
 
     </div>
   )

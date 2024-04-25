ac7ade0a7fd4b614a31ea863df1bb8aed6412136
AliAbdelAall <3laych@gmail.com>
2024-04-25 16:40:08 +0300

    styled LoginInput component in web

--- web/src/components/LoginInput/index.jsx
+++ web/src/components/LoginInput/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+// Styles
+import "./style.css"
+
 const LoginInput = ({id, label, placeholder}) => {
   return (
 
     <div className='flex column login-input'>
-      <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} placeholder={placeholder}/>
+      <label className='font-medium' htmlFor={id}>{label}</label>
+      <input className='font-light' type="text" id={id} placeholder={placeholder}/>
     </div>
   )
 }

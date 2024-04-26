58d88f1caa7f42cb08b7beaa93f86ff614fa673a
AliAbdelAall <3laych@gmail.com>
2024-04-26 13:58:58 +0300

    added functionalities in SendEmail component

--- web/src/pages/Auth/components/SendEmail/index.jsx
+++ web/src/pages/Auth/components/SendEmail/index.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 // Assets
 import fullLogo from "../../../../assets/logo/full-logo.png"
 
 // components
 import LoginInput from '../../../../components/LoginInput'
-import LoginButton from '../../../../components/LoginButton'
 import SmallButton from '../../../../components/SmallButton'
 
+// Utilities
+import { toast } from "react-toastify"
+import { useSendRequest } from '../../../../core/tools/remote/request'
+import { requestMethods } from '../../../../core/enums/requestMethods'
+
 const ResetPassword = () => {
+
+  const navigate = useNavigate()
+  const sendRequest = useSendRequest()
+
+  const [credentials, setCredetials] = useState({
+    email: "",
+
+  })
+
+  const [error, setError] = useState({
+    status: false,
+    message: ""
+  })
+
+  const handleInputChange = (e, field) => {
+    setCredetials({...credentials, [field]: e.target.value})
+    setError({ status: false, message: ""})
+  }
+
   return (
     <div className='flex column align-center login-container'>
       <img src={fullLogo} width={100} height={120} alt="logo" />
@@ -19,13 +43,13 @@ const ResetPassword = () => {
           <p className='text-acient'>A secret code will be sent to your email</p>
         </div>
 
-        {/* {error.status &&<p className='text-sm text-error'>{error.message}</p>} */}
+        {error.status &&<p className='text-sm text-error'>{error.message}</p>}
 
         <LoginInput
         id={"email-input"}
         label={"email"}
         placeholder={"jhondoe@gmail.com"}
-        // handleChange={(e) => handleInputChange(e, "email")}
+        handleChange={(e) => handleInputChange(e, "email")}
         />
 
       </div>

c3cf4c8a80e23f53dd4466344dd7e2f5cb3a9911
AliAbdelAall <3laych@gmail.com>
2024-04-26 19:52:50 +0300

    added ToastContainer

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,6 +6,10 @@ import "./styles/main.css"
 import "./styles/utilities.css"
 import "./styles/colors.css"
 
+// taostify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // Layouts
 import Auth from "./pages/Auth"
 
@@ -14,15 +18,29 @@ import Login from "./pages/Auth/components/Login"
 import Signup from "./pages/Auth/components/Signup"
 import ResetPassword from "./pages/Auth/components/ResetPassword"
 import SendEmail from "./pages/Auth/components/SendEmail"
+import VerifyOTP from "./pages/Auth/components/VerifyOTP"
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Routes>
         <Route path="/" element={<Auth/>}>
           <Route index element={<Login/>}/>
           <Route path="signup" element={<Signup/>}/>
           <Route path="send-email" element={<SendEmail/>}/>
+          <Route path="verify-otp" element={<VerifyOTP/>}/>
           <Route path="reset-password" element={<ResetPassword/>}/>
         </Route>
       </Routes>

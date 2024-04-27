48608ba5e62364d479c71750fcfd22f6e2cb8bb5
AliAbdelAall <3laych@gmail.com>
2024-04-27 17:17:38 +0300

    adeded props in ClientCard component

--- web/src/components/ClientCard/index.jsx
+++ web/src/components/ClientCard/index.jsx
@@ -3,33 +3,30 @@ import React from 'react'
 // Styles
 import "./style.css"
 
-// Assets
-import "../../assets/images/profile.jpg"
-
-const ClientCard = () => {
+const ClientCard = ({profilePic, name, age, phone, email, children = [], }) => {
   return (
     <div className='flex column center client-card'>
       <img className='client-image' src={profilePic} width={90}  height={90}  alt="profile" />
       <div className='flex column full-width client-info-wrapper'>
         <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Name</p>
-          <p className='font-light text-acient'>Jhon Doe</p>
+          <p className='font-light text-acient'>{name}</p>
         </div>
         <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Age</p>
-          <p className='font-light text-acient'>37 years old</p>
+          <p className='font-light text-acient'>{`${age} years old`}</p>
         </div>
         <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Children</p>
-          <p className='font-light text-acient'>Mathew, Andrew, Jolly</p>
+          <p className='font-light text-acient'>{children}</p>
         </div>
         <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Phone</p>
-          <p className='font-light text-acient'>+96170123456</p>
+          <p className='font-light text-acient'>{phone}</p>
         </div>
         <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Email</p>
-          <p className='font-light text-acient'>jhon@gmail.com</p>
+          <p className='font-light text-acient'>{email}</p>
         </div>
       </div>
     </div>

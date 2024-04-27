45b8cdea857d60b6f21b4d7210316b182d8a4fc3
AliAbdelAall <3laych@gmail.com>
2024-04-27 03:27:42 +0300

    added psychologist middleware

---
+++ backend/src/middlewares/psychologist.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express"
+import { prismaClient } from ".."
+
+const psychologistMiddleware = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const { id } = req.user!
+
+    const user = await prismaClient.user.findFirst({where:{id}})
+
+    if(!user || user.roleId !== 3){
+      return res.status(401).send("unauthorized")
+    }
+
+    next()
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send("Internal server error")
+  }
+}
+
+export default psychologistMiddleware

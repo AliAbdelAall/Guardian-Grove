248f713b321952639b360b4400649f20657ef6dd
AliAbdelAall <3laych@gmail.com>
2024-04-28 15:54:07 +0300

    connected redux to Clients component

--- web/src/pages/Main/components/Clients/index.jsx
+++ web/src/pages/Main/components/Clients/index.jsx
@@ -3,87 +3,52 @@ import React from 'react'
 // Assets
 import profilePic from "../../../../assets/images/profile.jpg"
 
+// Redux
+import { useSelector } from 'react-redux'
+import { parentsSliceName } from '../../../../core/redux/parents'
+import { childrenSliceName } from '../../../../core/redux/children'
+
 // Components
 import ClientCard from '../../../../components/ClientCard'
 
 const Clients = () => {
+
+  const { parents } = useSelector((global) => global[parentsSliceName])
+  const { children } = useSelector((global) => global[childrenSliceName])
+
   return (
-    <div className='flex column clients-cards-container'>
+    <div className='flex column full-width clients-cards-container'>
       <h2 className='text-acient'> My Clients </h2>
       
-      <div className='flex column clients-search-wrapper'>
+      <div className='flex column full-width clients-search-wrapper'>
         <div>
           <input className='search-input ' placeholder='Search' type="text" />
 
         </div>
-        <div className='flex full-width'>
-          <div className='flex wrap clients-cards-wrapper'>
-            <ClientCard
-            key={1}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={2}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={3}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={4}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={5}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={6}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
-            <ClientCard
-            key={7}
-            name={"John Doe"}
-            age={"37"}
-            profilePic={profilePic}
-            email={"jhon@gmail.com"}
-            phone={"+96170123456"}
-            children={"Mathew, Jolly, Nada"}
-            />
 
+          <div className='flex wrap clients-cards-wrapper'>
+
+            {parents?.map((parent) => {
+              const {id, profile} = parent
+              const parentChildren = children.filter((child) => child.parentId === parent.id)
+              const childrenNames = []
+              parentChildren.forEach(child => {childrenNames.push(child.name)})
+              return (
+                <ClientCard
+                key={id}
+                id = {profile.id}
+                name={`${profile.firstName} ${profile.lastName}`}
+                age={profile.dob}
+                profilePic={profilePic}
+                email={profile.email}
+                children={childrenNames}
+                />
+              )
+
+            })}
+           
           </div>
-        </div>
+
 
       </div>
     </div>

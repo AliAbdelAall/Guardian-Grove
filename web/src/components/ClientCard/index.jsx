f8ea94f415e6c7c470e6602b32ec508994b55888
AliAbdelAall <3laych@gmail.com>
2024-04-28 15:53:31 +0300

    updated ClientCard component

--- web/src/components/ClientCard/index.jsx
+++ web/src/components/ClientCard/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // Styles
 import "./style.css"
 
-const ClientCard = ({profilePic, name, age, phone, email, children = [], }) => {
+const ClientCard = ({profilePic, name, age, email, children = [], }) => {
   return (
     <div className='flex column center client-card'>
       <img className='client-image' src={profilePic} width={90}  height={90}  alt="profile" />
@@ -17,16 +17,12 @@ const ClientCard = ({profilePic, name, age, phone, email, children = [], }) => {
           <p className='font-light text-acient'>{`${age} years old`}</p>
         </div>
         <div className='flex space-between full-width client-info'>
-          <p className='font-medium text-acient'>Children</p>
-          <p className='font-light text-acient'>{children}</p>
-        </div>
-        <div className='flex space-between full-width client-info'>
-          <p className='font-medium text-acient'>Phone</p>
-          <p className='font-light text-acient'>{phone}</p>
-        </div>
-        <div className='flex space-between full-width client-info'>
           <p className='font-medium text-acient'>Email</p>
           <p className='font-light text-acient'>{email}</p>
+        </div>
+        <div className='flex space-between full-width client-info'>
+          <p className='font-medium text-acient'>Children</p>
+          <p className='font-light text-acient'>{children}</p>
         </div>
       </div>
     </div>

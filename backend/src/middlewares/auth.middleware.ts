9e84443b444747c99584445c032b43548746075f
AliAbdelAall <3laych@gmail.com>
2024-04-28 04:08:28 +0300

    fixed error handling in auth middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -17,24 +17,28 @@ const authMiddleware = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const token = req.headers.authorization?.split(" ")[1]
     if (!token) {
-      return res.status(401).send("Unauthorized");
+      return res.status(400).send("Invalid user");
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: number }
 
-    if (decoded) {
-      const user = await prismaClient.user.findFirst({
-        where: {
-          id: decoded.id,
-        },
-      })
-
-      if (user) {
-        req.user = { id: user.id }
-      }
+    if (!decoded) {
+      return res.status(401).send("Unauthenticated");
     }
+
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: decoded.id,
+      },
+    })
+
+    if (!user) {
+      return res.status(401).send("Unauthenticated");
+    }
+
+    req.user = { id: user.id,  }
     next()
   } catch (error) {
-    return res.status(401).send("unauthenticated")
+    return res.status(401).send("Unauthenticated")
   }
 }
 

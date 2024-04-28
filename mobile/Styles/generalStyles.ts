bbef3c194414d5573eef9cec562fe934d45eb4de
AliAbdelAall <3laych@gmail.com>
2024-04-29 02:15:17 +0300

    added generaStyles file

---
+++ mobile/Styles/generalStyles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  h3: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 6,
+  },
+})
+
+export default styles

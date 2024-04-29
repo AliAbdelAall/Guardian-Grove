a9c5e80855bea26cb362769e5074ce7382eb881b
AliAbdelAall <3laych@gmail.com>
2024-04-29 16:41:07 +0300

    backend: added AddStudent API in teacher controller

--- backend/src/Routes/teacher.routes.ts
+++ backend/src/Routes/teacher.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import teacherMiddleware from "../middlewares/teacher.middleware";
-import { getStudents } from "../controllers/teach.controller";
+import { addStudent, getStudents } from "../controllers/teach.controller";
 
 
 const teacherRoutes = Router()
 
 teacherRoutes.get("/get-students", authMiddleware, teacherMiddleware, getStudents)
+teacherRoutes.put("/add-student", authMiddleware, teacherMiddleware, addStudent)
 
 export default teacherRoutes
--- backend/src/controllers/teach.controller.ts
+++ backend/src/controllers/teach.controller.ts
@@ -2,6 +2,41 @@ import { Request, Response } from "express";
 import { prismaClient } from "..";
 
 
+export const addStudent = async (req:Request, res: Response) => {
+  try {
+    const {id} = req.user!
+
+    const {childId} = req.body
+
+    const teacher = await prismaClient.profile.findFirst({where:{userId:id}, include:{Teacher:true}})
+
+
+    await prismaClient.child.update({
+      where:{id: childId},
+      data:{teacherId: teacher?.Teacher?.id}
+    })
+
+    await prismaClient.teacher.update({
+      where:{profileId: teacher?.id},
+      data:{students:{connect: {id: childId}}}
+    })
+
+    const updatedTeacher = await prismaClient.teacher.findFirst({where:{profileId: teacher!.id}, include:{students: true}})
+    const updatedStudent = await prismaClient.child.findFirst({where:{id:childId}, include:{parent: {include:{profile: true}}}})
+
+    return res.status(200).json({
+      message: "student added successfuly",
+      teacher: updatedTeacher,
+      student: updatedStudent
+    })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({error: "Internal server error!"})
+  }
+}
+
+
 export const getStudents = async (req:Request, res: Response) => {
   try {
     const {id} = req.user!

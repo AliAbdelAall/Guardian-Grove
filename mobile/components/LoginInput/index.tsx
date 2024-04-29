a67e37fdf8d04d05fdc4149a33930dd6ff3740db
AliAbdelAall <3laych@gmail.com>
2024-04-30 00:43:11 +0300

    mobile: updated styles in LoginInput component

--- mobile/components/LoginInput/index.tsx
+++ mobile/components/LoginInput/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 
-const LoginInput = ({ value, handlechange, placeholder, password= false}) => {
+const LoginInput = ({ value, handlechange, placeholder, password= false , half = false}) => {
   return (
-    <View>
-       <TextInput
-          style={styles.loginButton}
-          value={value}
-          onChangeText={handlechange}
-          secureTextEntry={password}
-          placeholder={placeholder}
-        />
-    </View>
+    <TextInput
+      style={[styles.loginButton, half ? styles.halfWidth : null]}
+      value={value}
+      onChangeText={handlechange}
+      secureTextEntry={password}
+      placeholder={placeholder}
+    />
+
   )
 }
 
@@ -22,14 +21,19 @@ const styles = StyleSheet.create({
     borderColor: 'gray', 
     borderWidth: 1,
     borderRadius: 12,
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
+  fullWidth:{
+    width: '100%'
+  },
+  halfWidth:{
+    width: '50%',
+  },
+ 
 })
 
 const Styles = StyleSheet.create({
 
 })
-
-
 
 export default LoginInput

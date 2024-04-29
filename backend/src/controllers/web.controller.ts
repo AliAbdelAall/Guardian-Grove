f38fa882acb5e0a44cb8ce7d6a6ba387cf9e0148
AliAbdelAall <3laych@gmail.com>
2024-04-29 15:38:04 +0300

    backend: updated checkRole API

--- backend/src/controllers/web.controller.ts
+++ backend/src/controllers/web.controller.ts
@@ -5,9 +5,24 @@ export const checkRole = async (req:Request, res:Response) => {
   try {
     const {id} = req.user!
 
-    const user = await prismaClient.user.findFirst({where: {id}, include:{ role: true }})
-    
-    return res.status(200).json({role: user?.role.name})
+    const user = await prismaClient.user.findFirst({where: {id}, include:{ 
+      role: true
+    }})
+
+    if (user?.roleId === 3){
+      const psychologist =  await prismaClient.profile.findFirst({where:{userId: user.id}, include:{Psychologist:true}})
+      return res.status(200).json({
+        userRole: user?.role.name,
+        profile: psychologist
+      })
+    }
+    if (user?.roleId === 2){
+      const teacher = await prismaClient.profile.findFirst({where:{userId: user.id}, include:{Teacher:true}})
+      return res.status(200).json({
+        userRole: user?.role.name,
+        profile: teacher
+      })
+    }
 
   } catch (error) {
     console.log(error)

8d1219d9c44a24563c569ef47529e06db590d675
AliAbdelAall <3laych@gmail.com>
2024-04-30 00:42:33 +0300

    mobile: updated styles in Signup component

--- mobile/app/(Auth)/Signup.tsx
+++ mobile/app/(Auth)/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Image, Pressable, StyleSheet, Text, View, TextInput } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import {useRouter} from "expo-router"
 
 //  Styles
@@ -77,44 +77,44 @@ const Signup = () => {
 
     setError({status: false, message: ""})
 
+    console.log("before request")
+
     sendRequest(requestMethods.POST, "/api/auth/signup", {
       ...credentials,
     }).then((response) => {
       if(response.status === 201){
         setLocalUser(response.data.token)
         console.log(response.data)
-        router.replace("main")
+        // router.push("/Login")
       }
     }).catch((error) => {
       if(error.response.status === 400){
         setError({...error, status: true, message: error.response.data.error})
       }
     })
-  }
+  } 
 
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.image}></Image>
-      <View style={styles.center}>
+      <View style={[styles.center, styles.Width80]}>
         <Text style={generalStyles.h1}>Join us to start searching</Text>
-        <Text style={styles.textcenter}>
+        <Text style={[styles.textcenter, styles.lineHeight14]}>
           You can search course, apply course and find
           scholarship for abroad studies
         </Text>
       </View>
 
-      <View style={styles.gap}>
-        <View style={[styles.row, styles.fullWidth]}>
+      <View style={[styles.gap15, styles.fullWidth]}>
+        <View style={[styles.row, styles.fullWidth, styles.gap15]}>
           <LoginInput
+            half={true}
             value={credentials.firstName}
-            handlechange={(value:string) => {
-              console.log(value)
-              console.log(credentials)
-              handleInputChange(value, 'firstName')
-            }}
+            handlechange={(value:string) => {handleInputChange(value, 'firstName')}}
             placeholder={"First Name"}
           />
           <LoginInput
+            half={true}
             value={credentials.lastName}
             handlechange={(value:String) => handleInputChange(value, 'lastName')}
             placeholder={"Last Name"}
@@ -141,7 +141,11 @@ const Signup = () => {
           handlePress={handleSignupValidation}
         />
 
-        <Text style={styles.textcenter}>Already have an account? <Text>Login</Text></Text>
+        <Text style={[styles.textcenter, styles.fontSize16]}>{'Already have an account? '}
+          <Pressable onPress={()=>{router.push("/app/Login")}}>
+            <Text style={[styles.fontMedium, styles.primaryColor]}>Login</Text>
+          </Pressable>
+        </Text>
 
       </View>
     </View>
@@ -170,7 +174,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   fullWidth:{
-    width: '100%'
+    minWidth: '100%'
+  },
+  Width80:{
+    width: '80%',
+  },
+  lineHeight14:{
+    lineHeight:24,
+    marginBottom:10
+  },
+
+  autoStretch:{
+    flex:1
   },
   loginButton:{
     height:55,
@@ -180,14 +195,23 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     width: "auto"
   },
-
-  gap:{
+  gap15:{
     gap:15
-
   },
-
   textcenter:{
     textAlign: 'center'
+  },
+  backgroundRed:{
+    backgroundColor: "red"
+  },
+  fontMedium:{
+    fontWeight: '500'
+  },
+  primaryColor:{
+    color: '#75AB19'
+  },
+  fontSize16:{
+    fontSize: 16
   }
   
 })

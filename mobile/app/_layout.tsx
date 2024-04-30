316faac1764fca6d1cfef6ab2a234999b42aea15
AliAbdelAall <3laych@gmail.com>
2024-04-30 19:42:53 +0300

    mobile: updated RootLayout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,11 +5,19 @@ import { View } from 'react-native'
 const RootLayout = () => {
   return (
     <Stack>
+      <Stack.Screen name='index' options={{
+        headerShown: false 
+        }
+      }/>
       <Stack.Screen name='(Auth)' options={{
         headerShown: false 
-      }
-      }></Stack.Screen>
-      <Stack.Screen name='home'></Stack.Screen>
+        }
+      }/>
+      <Stack.Screen name='home' options={{
+        // headerShown: false 
+        }
+      }/>
+
     </Stack>
   )
 }

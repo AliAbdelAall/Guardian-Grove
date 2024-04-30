789e99f1342e5fbf3e4c999b18c2cdb540fcebb4
AliAbdelAall <3laych@gmail.com>
2024-04-30 21:51:09 +0300

    mobile: added psychologists section in Home page

--- mobile/app/home/Home.tsx
+++ mobile/app/home/Home.tsx
@@ -8,6 +8,32 @@ const heroImage = require("../../assets/images/hero-image.png")
 
 const Main = () => {
 
+  const psychologists = [
+    {
+    profilePic,
+    name: "Jhon Doe",
+    speciality: 'development',
+    rating: 4
+    },
+    {
+      profilePic,
+      name: "Jhonny Donny",
+      speciality: 'family',
+      rating: 3.5
+    },
+    {
+    profilePic,
+    name: "Ahmad Fakih",
+    speciality: 'development',
+    rating: 4.5
+    },
+    {
+      profilePic,
+      name: "Lilly barney",
+      speciality: 'family',
+      rating: 4.5
+    },
+  ]
 
   return (
     <View style={utilities.container}>
@@ -25,7 +51,32 @@ const Main = () => {
           </View>
         </View>
 
+        <View style={styles.sectionTitleWrapper}>
+          <Text style={styles.sectionTitle}>Popular Specialists</Text>
+          <Pressable>
+            <Text style={styles.seeAll}>See All</Text>
+          </Pressable>
+        </View>
 
+        <FlatList
+        horizontal={true}
+        data={psychologists}
+        ItemSeparatorComponent={() => {
+          return <View style={styles.separator}></View>
+        }} 
+        renderItem={(element) => {
+          return (
+          <View style ={styles.psychologistCardWrapper}>
+            <Image source={element.item.profilePic}  style ={styles.psychologistCardImage}></Image>
+            <View style={styles.psychologistCardInfoWrapper}>
+              <Text style={styles.psychologistCardName}>{`Dr. ${element.item.name}`}</Text>
+              <Text style={styles.psychologistCardSpeciality}>{`${element.item.speciality} specialist`}</Text>
+              <Text style={styles.psychologistCardRating}>{`Rating: ${element.item.rating}`}</Text>
+            </View>
+          </View>)
+        }
+          
+        }/>
 
 
       </ScrollView>

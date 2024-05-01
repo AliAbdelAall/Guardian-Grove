77a85d53dddd0d29139bd70c0f55c5bac99c7cf5
AliAbdelAall <3laych@gmail.com>
2024-05-01 16:28:49 +0300

    backend: added model Review + reset migration

--- backend/prisma/migrations/20240427134603_create_profile_pic_users_column/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `passwordreset` ALTER COLUMN `expiresAt` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `profile` ADD COLUMN `profilePic` VARCHAR(191) NOT NULL DEFAULT 'default-profile';

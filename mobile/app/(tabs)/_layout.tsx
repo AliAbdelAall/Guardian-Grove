ec4f0a58179b0940447c0ce21209c476dd292a3c
AliAbdelAall <3laych@gmail.com>
2024-05-01 21:15:09 +0300

    backend: added HomeLayout

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack, Tabs } from "expo-router";
+import React from "react";
+
+const HomeLayout = () => {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name="Home"
+				options={{
+					title: "Home",
+					headerShown: false,
+				}}
+			/>
+			<Tabs.Screen
+				name="Profile"
+				options={{
+					title: "Profile",
+					headerShown: false,
+				}}
+			/>
+		</Tabs>
+	);
+};
+
+export default HomeLayout;

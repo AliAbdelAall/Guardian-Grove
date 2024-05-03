3c559dfd5a06e989e16e19fe97d61dfbfb42f06e
AliAbdelAall <3laych@gmail.com>
2024-05-03 15:12:04 +0300

    mobile: added redux provider for homeLayout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,28 +1,26 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../core/redux/store";
 
 const RootLayout = () => {
 	return (
-		<Stack>
-			<Stack.Screen
-				name="index"
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name="(Auth)"
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name="(tabs)"
-				options={{
-					headerShown: false,
-				}}
-			/>
-		</Stack>
+		<Provider store={store}>
+			<Stack>
+				<Stack.Screen
+					name="(Auth)"
+					options={{
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name="(tabs)"
+					options={{
+						headerShown: false,
+					}}
+				/>
+			</Stack>
+		</Provider>
 	);
 };
 

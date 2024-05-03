a94d53e43fd4f0a1a8f9dc4c541a39993f1ffb5b
AliAbdelAall <3laych@gmail.com>
2024-05-03 15:12:53 +0300

    mobile: added loadPsychologistAndTeachers function in HomeLayout

--- mobile/app/(tabs)/Home/_layout.tsx
+++ mobile/app/(tabs)/Home/_layout.tsx
@@ -1,7 +1,41 @@
 import { Stack, Tabs } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
+import { useSendRequest } from "../../../core/tools/remote/request";
+import { requestMethods } from "../../../core/enum/requestMetods";
+import { setTeachers } from "../../../core/redux/teachers";
+import { setpsychologists } from "../../../core/redux/Psychologists";
+import Toast from "react-native-toast-message";
+import { useDispatch } from "react-redux";
 
 const HomeLayout = () => {
+	const dispatch = useDispatch();
+	const sendRequest = useSendRequest();
+
+	useEffect(() => {
+		loadPsychologistAndTeachers();
+	}, []);
+
+	const loadPsychologistAndTeachers = () => {
+		sendRequest(
+			requestMethods.GET,
+			"/api/parent/get-psychologists-teachers"
+		)
+			.then((response) => {
+				if (response.status === 200) {
+					console.log(response.data);
+					dispatch(setTeachers(response.data.teachers));
+					dispatch(setpsychologists(response.data.psychologists));
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				Toast.show({
+					type: "error",
+					text1: "Something went Wrong!",
+				});
+			});
+	};
+
 	return (
 		<Stack>
 			<Stack.Screen

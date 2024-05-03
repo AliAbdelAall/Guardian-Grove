0cc51449daa90a14eee5eaf732cb1fb721f935d5
AliAbdelAall <3laych@gmail.com>
2024-05-03 15:16:01 +0300

    backend: updated type for initialState in redux childrenSlice

--- mobile/core/redux/children/index.ts
+++ mobile/core/redux/children/index.ts
@@ -1,26 +1,27 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface child {
-  id: number
-  parentId: number
-  name: string,
-  school: string,
-  dob: String | null
+	id: number;
+	parentId: number;
+	teacherId: string | null;
+	profilePic: String;
+	name: string;
+	school: string;
+	dob: String | null;
 }
 
-const initialState: child[] = []
+const initialState: child[] = [];
 
 const childrenSlice = createSlice({
-  initialState,
-  name: "childrenSlice",
-  reducers:{
-    setChildren: (state, action: PayloadAction<child[]>) => {
-      return action.payload
-    }
-  }
-})
+	initialState,
+	name: "childrenSlice",
+	reducers: {
+		setChildren: (state, action: PayloadAction<child[]>) => {
+			return action.payload;
+		},
+	},
+});
 
-
-export const { setChildren } = childrenSlice.actions
-export const childrenSliceName = childrenSlice.name
-export default childrenSlice.reducer
+export const { setChildren } = childrenSlice.actions;
+export const childrenSliceName = childrenSlice.name;
+export default childrenSlice.reducer;

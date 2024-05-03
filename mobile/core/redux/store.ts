5a256f7300d244490129587e8f2ce96270a6d730
AliAbdelAall <3laych@gmail.com>
2024-05-03 11:16:10 +0300

    mobile: added redux teachersSlice

--- mobile/core/redux/store.ts
+++ mobile/core/redux/store.ts
@@ -1,19 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer, { userSliceName } from "./user/index."
-import childrenReducer, { childrenSliceName } from "./children"
-import psychologistsReducer, { psychologistsSliceName } from "./Psychologists"
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer, { userSliceName } from "./user/index.";
+import childrenReducer, { childrenSliceName } from "./children";
+import psychologistsReducer, { psychologistsSliceName } from "./Psychologists";
+import teachersReducer, { teachersSliceName } from "./teachers";
 
 // Logger
-import logger from "redux-logger"
+import logger from "redux-logger";
 
 export const store = configureStore({
-  reducer: {
-    [userSliceName]: userReducer,
-    [childrenSliceName]: childrenReducer,
-    [psychologistsSliceName]: psychologistsReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-})
+	reducer: {
+		[userSliceName]: userReducer,
+		[childrenSliceName]: childrenReducer,
+		[psychologistsSliceName]: psychologistsReducer,
+		[teachersSliceName]: teachersReducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
-export type RootState =  ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ mobile/core/redux/teachers/index.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface teacher {
+  id: number
+  firstName: string,
+  lastName: string,
+  email: string
+  profilePic: string
+  dob: String | null
+  speciality: string | null
+  school: string | null
+  rating: number | null
+}
+
+const initialState: teacher[] = []
+
+const teachersSlice = createSlice({
+  initialState,
+  name: "teachersSlice",
+  reducers:{
+    setTeachers: (state, action) => {
+      return {
+        ...action.payload
+      }
+    }
+  }
+})
+
+
+export const { setTeachers } = teachersSlice.actions
+export const teachersSliceName = teachersSlice.name
+export default teachersSlice.reducer

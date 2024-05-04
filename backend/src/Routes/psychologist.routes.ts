32b91c26de3b0849a538165952ad9b9befe5c898
AliAbdelAall <3laych@gmail.com>
2024-05-05 01:35:14 +0300

    backend: added updateSpeciality API in psuchologist controller

--- backend/src/Routes/psychologist.routes.ts
+++ backend/src/Routes/psychologist.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import psychologistMiddleware from "../middlewares/psychologist.middleware";
-import { getClients } from "../controllers/psychologist.controller";
+import {
+	getClients,
+	updateSpeciality,
+} from "../controllers/psychologist.controller";
 
-const psychologistRoutes = Router()
+const psychologistRoutes = Router();
 
-psychologistRoutes.get('/get-clients', authMiddleware, psychologistMiddleware, getClients)
+psychologistRoutes.get(
+	"/get-clients",
+	authMiddleware,
+	psychologistMiddleware,
+	getClients
+);
+psychologistRoutes.post(
+	"/update-speciality",
+	authMiddleware,
+	psychologistMiddleware,
+	updateSpeciality
+);
 
-export default psychologistRoutes
+export default psychologistRoutes;
--- backend/src/controllers/psychologist.controller.ts
+++ backend/src/controllers/psychologist.controller.ts
@@ -1,38 +1,69 @@
-import { Request, Response } from "express"
-import { prismaClient } from ".."
+import { Request, Response } from "express";
+import { prismaClient } from "..";
 
-export const getClients = async (req:Request, res:Response) => {
-  try {
-    const {id} = req.user!
+export const getClients = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.user!;
 
-    const profile = await prismaClient.profile.findFirst({
-      where:{
-        userId: id
-      }, 
-      include:{
-        Psychologist:{
-          include:{
-            clients: {
-              include:{
-                profile: true,
-                children: true
-              }
-            }
-          }
-        }
-      }
-    })
-    if(!profile || !profile.Psychologist){
-      return res.status(400).json({error: "Unauthenticated"})
-    }
-    const clients = profile.Psychologist.clients
-    
-    return res.status(200).json({
-      clients,
-    })
-    
-  } catch (error) {
-    console.log(error)
-    return res.status(500).json({error: "Internal server error!"})
-  }
-}
+		const profile = await prismaClient.profile.findFirst({
+			where: {
+				userId: id,
+			},
+			include: {
+				Psychologist: {
+					include: {
+						clients: {
+							include: {
+								profile: true,
+								children: true,
+							},
+						},
+					},
+				},
+			},
+		});
+		if (!profile || !profile.Psychologist) {
+			return res.status(400).json({ error: "Unauthenticated" });
+		}
+		const clients = profile.Psychologist.clients;
+
+		return res.status(200).json({
+			clients,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Internal server error!" });
+	}
+};
+
+export const updateSpeciality = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.user!;
+
+		const { speciality } = req.body;
+
+		if (
+			!speciality ||
+			(speciality !== "Family" && speciality !== "Development")
+		) {
+			return res.status(400).json({ error: "Invalid speciality" });
+		}
+
+		const user = await prismaClient.profile.findFirst({
+			where: { userId: id },
+		});
+
+		await prismaClient.psychologist.update({
+			where: { profileId: user?.id },
+			data: { speciality },
+		});
+
+		return res.status(200).json({
+			message: "speciality updated successfully",
+			speciality,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Internal server error!" });
+	}
+};

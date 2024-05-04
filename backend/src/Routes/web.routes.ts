cc7d526bed313b73ada55c38fd9fbfb3e09295a7
AliAbdelAall <3laych@gmail.com>
2024-05-04 20:25:12 +0300

    backend: added UpdateUserProfilePic API in web controller

--- backend/src/Routes/web.routes.ts
+++ backend/src/Routes/web.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import webMiddleware from "../middlewares/web.middleware";
-import { checkRole } from "../controllers/web.controller";
+import { UpdateUserProfilePic, checkRole } from "../controllers/web.controller";
+import multerMiddlware from "../middlewares/multer.middlware";
 
-const webRoutes = Router()
+const webRoutes = Router();
 
-webRoutes.get("/check-role", authMiddleware, webMiddleware, checkRole)
+webRoutes.get("/check-role", authMiddleware, webMiddleware, checkRole);
+webRoutes.post(
+	"/update-profile-picture",
+	authMiddleware,
+	multerMiddlware,
+	UpdateUserProfilePic
+);
 
-export default webRoutes
+export default webRoutes;
--- backend/src/controllers/web.controller.ts
+++ backend/src/controllers/web.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import fs from "fs/promises";
 
 export const checkRole = async (req: Request, res: Response) => {
 	try {
@@ -32,6 +33,46 @@ export const checkRole = async (req: Request, res: Response) => {
 				profile: teacher,
 			});
 		}
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Internal server error!" });
+	}
+};
+
+export const UpdateUserProfilePic = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.user!;
+		if (!req.file) {
+			return res.status(400).json({ error: "No file uploaded" });
+		}
+
+		const user = await prismaClient.user.findFirst({
+			where: { id },
+			include: { profile: true },
+		});
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const newProfilePic = req.file.filename;
+		const newProfilePicPath = req.file.path;
+
+		if (user.profile && user.profile.profilePic !== "default-profile.png") {
+			await fs.unlink(
+				`public/profile-pictures/${user.profile.profilePic}`
+			);
+		}
+
+		await prismaClient.profile.update({
+			where: { id: user.profile!.id },
+			data: { profilePic: newProfilePic },
+		});
+
+		return res.status(200).json({
+			message: "Profile picture updated successfully",
+			profilePic: newProfilePic,
+		});
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({ error: "Internal server error!" });

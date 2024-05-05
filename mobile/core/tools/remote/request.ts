24b687bf25c6f4ffa8badb1768d875f5d3559043
AliAbdelAall <3laych@gmail.com>
2024-05-05 21:52:41 +0300

    mobile: adjusted axios request hook to dynamically switch between multipart formdt and appliction/json

--- mobile/core/tools/remote/request.ts
+++ mobile/core/tools/remote/request.ts
@@ -1,42 +1,46 @@
-import axios, { AxiosResponse } from "axios"
-import { getLocalUser, removeLocalUser } from "../local/user"
-import {useRouter} from "expo-router"
+import axios, { AxiosResponse } from "axios";
+import { getLocalUser, removeLocalUser } from "../local/user";
+import { useRouter } from "expo-router";
 
-axios.defaults.baseURL = "http://192.168.0.201:3000"
+axios.defaults.baseURL = "http://192.168.0.201:3000";
 
 interface ErrorResponse {
-  response: {
-    status: number;
-  }
+	response: {
+		status: number;
+	};
 }
 
 export const useSendRequest = () => {
-  const router = useRouter()
+	const router = useRouter();
 
-  const sendRequest = async (method: string, route: string, body?: any) => {
-    try {
-      const token = await getLocalUser() ?? ""
-      console.log("request is here")
-      const response: AxiosResponse = await axios.request({
-        method,
-        url: route,
-        data: body,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      })
-      console.log(response)
-      return response
-    } catch (error) {
-      const { status } = (error as ErrorResponse).response;
-      if (status === 401) {
-        await removeLocalUser()
-        router.dismissAll
-      }
-      console.error(error)
-      throw error
-    }
-  }
-  return sendRequest
-}
+	const sendRequest = async (method: string, route: string, body?: any) => {
+		try {
+			const token = (await getLocalUser()) ?? "";
+			const headers = {
+				Authorization: `bearer ${token}`,
+			};
+			if (body instanceof FormData) {
+				headers["Content-Type"] = "multipart/form-data";
+			} else {
+				headers["Content-Type"] = "application/json";
+			}
+			const response: AxiosResponse = await axios.request({
+				method,
+				url: route,
+				data: body,
+				headers,
+			});
+			console.log(response);
+			return response;
+		} catch (error) {
+			const { status } = (error as ErrorResponse).response;
+			if (status === 401) {
+				await removeLocalUser();
+				router.dismissAll;
+			}
+			console.error(error);
+			throw error;
+		}
+	};
+	return sendRequest;
+};

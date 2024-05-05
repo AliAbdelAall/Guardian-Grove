4a40d5221cd7132a4624b3959b0fd748779a0506
AliAbdelAall <3laych@gmail.com>
2024-05-05 20:09:24 +0300

    mobile: added props type in PsychologistCard component

--- mobile/components/PsychologistCard/index.tsx
+++ mobile/components/PsychologistCard/index.tsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import { Image, Text, View } from 'react-native'
-import {StarRatingDisplay} from 'react-native-star-rating-widget';
+import React, { FC } from "react";
+import { Image, Text, View } from "react-native";
+import { StarRatingDisplay } from "react-native-star-rating-widget";
 
 // Styles
-import { styles } from './styles'
+import { styles } from "./styles";
 
-const PsychologistCard = ({profilePic, name, speciality, rating}) => {
-  return (
-    <View style ={styles.psychologistCardWrapper}>
-    <Image source={profilePic}  style ={styles.psychologistCardImage}></Image>
-    <View style={styles.psychologistCardInfoWrapper}>
-      <Text style={styles.psychologistCardName}>{`Dr. ${name}`}</Text>
-      <Text style={styles.psychologistCardSpeciality}>{`${speciality} specialist`}</Text>
-      <StarRatingDisplay
-        rating={rating}
-        maxStars={5}
-        starSize={20}
-        enableHalfStar={true}
-        emptyColor="#C4C4C4"
-        starStyle={styles.starStyle}
-        style={styles.starRatingDisplay}
-      />
-    </View>
-  </View>
-  )
-}
+type props = {
+	id: number;
+	profilePic: string;
+	name: string;
+	rating: number | null;
+	speciality: string | null;
+};
 
+const PsychologistCard: FC<props> = ({
+	id,
+	profilePic,
+	name,
+	speciality,
+	rating,
+}) => {
+	return (
+		<View style={styles.psychologistCardWrapper}>
+			<Image
+				src={profilePic}
+				style={styles.psychologistCardImage}
+			></Image>
+			<View style={styles.psychologistCardInfoWrapper}>
+				<Text style={styles.psychologistCardName}>{`Dr. ${name}`}</Text>
+				<Text
+					style={styles.psychologistCardSpeciality}
+				>{`${speciality} specialist`}</Text>
+				<StarRatingDisplay
+					rating={rating}
+					maxStars={5}
+					starSize={20}
+					enableHalfStar={true}
+					emptyColor="#C4C4C4"
+					starStyle={styles.starStyle}
+					style={styles.starRatingDisplay}
+				/>
+			</View>
+		</View>
+	);
+};
 
-
-export default PsychologistCard
+export default PsychologistCard;

2eb4248c7387a074cf77d73265cac7cfe5785120
AliAbdelAall <3laych@gmail.com>
2024-05-05 16:05:57 +0300

    web: added dispatch to save schools in redux schoolsSlice

--- web/src/pages/Main/index.jsx
+++ web/src/pages/Main/index.jsx
@@ -1,113 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 // Styles
-import "./style.css"
+import "./style.css";
 
 // Redux
-import { useDispatch } from "react-redux"
-import { setchildren } from '../../core/redux/children'
-import { setParents } from '../../core/redux/parents'
-import { setProfile } from '../../core/redux/userProfile'
+import { useDispatch } from "react-redux";
+import { setchildren } from "../../core/redux/children";
+import { setParents } from "../../core/redux/parents";
+import { setProfile } from "../../core/redux/userProfile";
 
 // Tools
-import { useSendRequest } from '../../core/tools/remote/request'
-import { requestMethods } from '../../core/enums/requestMethods'
+import { useSendRequest } from "../../core/tools/remote/request";
+import { requestMethods } from "../../core/enums/requestMethods";
 
 // Toastify
-import { toast } from 'react-toastify'
+import { toast } from "react-toastify";
 
 // Components
-import Sidebar from './components/Sidebar'
-
+import Sidebar from "./components/Sidebar";
+import { setSchools } from "../../core/redux/shcools";
 
 const Main = () => {
+	const navigate = useNavigate();
+	const sendRequest = useSendRequest();
+	const dispatch = useDispatch();
+	const [role, setRole] = useState("");
 
-  const navigate = useNavigate()
-  const sendRequest = useSendRequest()
-  const dispatch = useDispatch()
-  const[ role, setRole] = useState("")
+	useEffect(() => {
+		checkRole();
+	}, []);
 
-  useEffect(()=>{
-    checkRole()
-  },[])
+	const checkRole = () => {
+		sendRequest(requestMethods.GET, "/api/web/check-role")
+			.then((response) => {
+				if (response.status === 200) {
+					const { userRole, profile } = response.data;
+					setRole(userRole);
+					dispatch(setProfile(profile));
+					userRole === "Teacher" ? loadStudents() : loadClients();
+				}
+			})
+			.catch((error) => {
+				toast.error("something went wrong...");
+			});
+	};
 
-  const checkRole = () => {
-    sendRequest(requestMethods.GET, "/api/web/check-role").then((response) => {
-      if(response.status === 200){
-        const { userRole, profile } = response.data
-        setRole(userRole)
-        dispatch(setProfile(profile))
-        userRole === "Teacher" ? loadStudents() : loadClients()
-        
-      }
-    }).catch((error) => {
-      toast.error("something went wrong...")
-  })
-  }
+	const loadStudents = () => {
+		navigate("/main/teacher");
+		sendRequest(requestMethods.GET, "/api/teacher/get-students")
+			.then((response) => {
+				if (response.status === 200) {
+					console.log(response.data);
+					const students = response.data.students;
+					const parentsList = [];
+					const studentsList = [];
 
-  const loadStudents = () => {
-    navigate("/main/teacher")
-    sendRequest(requestMethods.GET, "/api/teacher/get-students").then((response) => {
-      if(response.status === 200){
-        console.log(response.data)
-        const students = response.data.students
-        const parentsList = []
-        const studentsList = []
+					students?.forEach((student) => {
+						console.log(student);
+						const { parent, ...studentInfo } = student;
+						parentsList.push(parent);
+						console.log(students);
+						studentsList.push(studentInfo);
+					});
+					console.log(studentsList);
+					console.log(parentsList);
+					dispatch(setParents(parentsList));
+					dispatch(setchildren(studentsList));
+					dispatch(setSchools(response.data.schools));
+					toast.success("load successful");
+				}
+			})
+			.catch((error) => {
+				toast.error("something went wrong...");
+			});
+	};
 
-        students?.forEach((student) => {
-          console.log(student)
-          const {parent, ...studentInfo} = student
-          parentsList.push(parent)
-          console.log(students)
-          studentsList.push(studentInfo)
-        })
-        console.log(studentsList)
-        console.log(parentsList)
-        dispatch(setParents(parentsList))
-        dispatch(setchildren(studentsList))
-        toast.success("load successful")
-      }
-      }).catch((error) => {
-        toast.error("something went wrong...")
-      })
-  }
+	const loadClients = () => {
+		navigate("/main/psychologist");
+		sendRequest(requestMethods.GET, "/api/psychologist/get-clients")
+			.then((response) => {
+				if (response.status === 200) {
+					console.log(response.data);
+					const clients = response.data.clients;
+					const parentsList = [];
+					const childrenList = [];
 
-  const loadClients = () => {
-    navigate("/main/psychologist")
-    sendRequest(requestMethods.GET, "/api/psychologist/get-clients").then((response) => {
-      if(response.status === 200){
-        console.log(response.data)
-        const clients = response.data.clients
-        const parentsList = []
-        const childrenList = []
+					clients?.forEach((client) => {
+						console.log(client);
+						const { children, ...parent } = client;
+						parentsList.push(parent);
+						console.log(children);
+						childrenList.push(...children);
+					});
+					console.log(childrenList);
+					console.log(parentsList);
+					dispatch(setParents(parentsList));
+					dispatch(setchildren(childrenList));
+					toast.success("load successful");
+				}
+			})
+			.catch((error) => {
+				toast.error("something went wrong...");
+			});
+	};
 
-        clients?.forEach((client) => {
-          console.log(client)
-          const {children, ...parent} = client
-          parentsList.push(parent)
-          console.log(children)
-          childrenList.push(...children)
-        })
-        console.log(childrenList)
-        console.log(parentsList)
-        dispatch(setParents(parentsList))
-        dispatch(setchildren(childrenList))
-        toast.success("load successful")
-      }
-      }).catch((error) => {
-        toast.error("something went wrong...")
-      })
-  }
+	return (
+		<div className="flex ">
+			<Sidebar role={role} />
+			<Outlet />
+		</div>
+	);
+};
 
-
-  return (
-    <div className='flex '>
-      <Sidebar role={role}/>
-      <Outlet/>
-    </div>
-
-  )
-}
-
-export default Main
+export default Main;

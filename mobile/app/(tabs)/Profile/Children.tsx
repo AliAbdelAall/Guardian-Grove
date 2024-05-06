cc3c7409186171e79138534625bb77eca20ac520
AliAbdelAall <3laych@gmail.com>
2024-05-06 20:42:06 +0300

    mobile: added addChild modal in Children component

--- mobile/app/(tabs)/Profile/Children.tsx
+++ mobile/app/(tabs)/Profile/Children.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FlatList, Image, ScrollView, Text, View } from "react-native";
+import { FlatList, Image, Modal, ScrollView, Text, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { Picker } from "@react-native-picker/picker";
 
 // Styles
 import { childrenStyles } from "../../../Styles/main/childrenStyles";
@@ -11,9 +12,17 @@ import LoginButton from "../../../components/LoginButton";
 import Child from "../../../components/Child";
 
 // Redux
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../core/redux/store";
-import { childrenSliceName } from "../../../core/redux/children";
+import { addChild, childrenSliceName } from "../../../core/redux/children";
+import LoginInput from "../../../components/LoginInput";
+import DateInput from "../../../components/DateInput";
+
+// Tools
+import { useSendRequest } from "../../../core/tools/remote/request";
+import { requestMethods } from "../../../core/enum/requestMetods";
+import { schoolSliceName } from "../../../core/redux/schools";
+import Toast from "react-native-toast-message";
 
 // Assets
 const ProfilePic = require("../../../assets/profile/profile.jpg");
@@ -22,9 +31,16 @@ const Children = () => {
 	const children = useSelector(
 		(global: RootState) => global[childrenSliceName]
 	);
-	console.log(
-		`${process.env.EXPO_PUBLIC_PROFILE_PICS_URL}${"default-profile.png"}`
-	);
+	const schools = useSelector((global: RootState) => global[schoolSliceName]);
+	const [childInfo, setChildInfo] = useState({
+		name: "",
+		dob: "",
+		schoolId: 0,
+	});
+	const [showPopoup, setShowPopoup] = useState(false);
+	const dispatch = useDispatch();
+	const sendRequest = useSendRequest();
+
 	return (
 		<ScrollView style={childrenStyles.childrenContainer}>
 			<View>
@@ -34,8 +50,90 @@ const Children = () => {
 						your children
 					</Text>
 				</View>
+
+				<Modal
+					animationType="fade"
+					transparent={true}
+					visible={showPopoup}
+				>
+					<View style={childrenStyles.overlay}></View>
+				</Modal>
+
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={showPopoup}
+					onRequestClose={() => {}}
+				>
+					<View style={childrenStyles.popupContainer}>
+						<View style={childrenStyles.popupWrapper}>
+							<LoginButton
+								handlePress={() => {}}
+								text={"Upload Image"}
+							/>
+							<LoginInput
+								key={"Name"}
+								placeholder={"Name"}
+								handlechange={(e: string) =>
+									setChildInfo({ ...childInfo, name: e })
+								}
+								value={childInfo.name}
+							/>
+							<DateInput
+								dob={childInfo.dob}
+								prevDob={childInfo.dob}
+								handleDateConfirmation={handleDateConfirmation}
+							/>
+							<View style={{ borderWidth: 1, borderRadius: 10 }}>
+								<Picker
+									selectedValue={childInfo.schoolId}
+									onValueChange={handleSchoolChange}
+								>
+									<Picker.Item
+										style={{
+											color: "#75AB19",
+											fontSize: 18,
+										}}
+										label="Choose School"
+										value={0}
+										key={0}
+									/>
+									{schools?.map((school) => {
+										const { id, name } = school;
+										return (
+											<Picker.Item
+												style={{
+													color: "#677294",
+													fontSize: 18,
+												}}
+												key={id}
+												label={name}
+												value={id}
+											/>
+										);
+									})}
+								</Picker>
+							</View>
+							<View style={childrenStyles.buttonsContainer}>
+								<View style={childrenStyles.halfButton}>
+									<LoginButton
+										handlePress={() => setShowPopoup(false)}
+										text={"Cancel"}
+									/>
+								</View>
+								<View style={childrenStyles.halfButton}>
+									<LoginButton
+										handlePress={() => setShowPopoup(false)}
+										text={"Confirm"}
+									/>
+								</View>
+							</View>
+						</View>
+					</View>
+				</Modal>
 				<FlatList
 					data={children}
+					scrollEnabled={false}
 					renderItem={(element) => {
 						const { id, name, profilePic, school, dob } =
 							element.item;
@@ -51,7 +149,10 @@ const Children = () => {
 					}}
 				></FlatList>
 			</View>
-			<LoginButton text={"Add Child"} handlePress={() => {}} />
+			<LoginButton
+				text={"Add Child"}
+				handlePress={() => setShowPopoup(true)}
+			/>
 		</ScrollView>
 	);
 };

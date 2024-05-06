f87087d6b5c1762a07a672c1c415dfa49d43efae
AliAbdelAall <3laych@gmail.com>
2024-05-07 01:41:44 +0300

    mobile: added props type and adjusted styleing in LoginInput component

--- mobile/components/LoginInput/index.tsx
+++ mobile/components/LoginInput/index.tsx
@@ -1,40 +1,47 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
-
-const LoginInput = ({ value, handlechange, placeholder, password= false , half = false}) => {
-  return (
-    <TextInput
-      style={[styles.loginButton, half ? styles.halfWidth : null]}
-      value={value}
-      onChangeText={handlechange}
-      secureTextEntry={password}
-      placeholder={placeholder}
-      placeholderTextColor={'#677294'}
-    />
-  )
-}
+import React, { FC } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+type Props = {
+	value: string;
+	handlechange: (value: String) => void;
+	placeholder: string;
+	password?: boolean;
+	half?: boolean;
+};
+const LoginInput: FC<Props> = ({
+	value,
+	handlechange,
+	placeholder,
+	password = false,
+	half = false,
+}) => {
+	return (
+		<TextInput
+			style={[styles.loginInput, half ? styles.halfWidth : null]}
+			value={value}
+			onChangeText={handlechange}
+			secureTextEntry={password}
+			placeholder={placeholder}
+			placeholderTextColor={"#B3B9CA"}
+		/>
+	);
+};
 
 const styles = StyleSheet.create({
-  loginButton:{
-    height:50,
-    paddingLeft: 15,
-    borderColor: '#B3B9CA', 
-    borderWidth: 1,
-  
-    borderRadius: 12,
-    fontSize: 18,
-  },
-  fullWidth:{
-    width: '100%'
-  },
-  halfWidth:{
-    width: '48.4%',
-  },
- 
-})
+	loginInput: {
+		height: 50,
+		paddingLeft: 15,
+		borderColor: "#B3B9CA",
+		borderWidth: 1,
+		color: "#677294",
+		borderRadius: 12,
+		fontSize: 18,
+	},
+	fullWidth: {
+		width: "100%",
+	},
+	halfWidth: {
+		width: "48.4%",
+	},
+});
 
-const Styles = StyleSheet.create({
-
-})
-
-export default LoginInput
+export default LoginInput;

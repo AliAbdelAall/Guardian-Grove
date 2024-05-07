6c6045f9acc9eaaabfa0fc8458140cdec6cf91da
AliAbdelAall <3laych@gmail.com>
2024-05-07 19:21:34 +0300

    mobile: styled FilterButton component

---
+++ mobile/components/FilterButton/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+	activeFilter: {
+		borderWidth: 1,
+		borderColor: "#75AB19",
+		backgroundColor: "#75AB19",
+		paddingHorizontal: 15,
+		paddingVertical: 5,
+		borderRadius: 13,
+	},
+	activeFilterText: {
+		fontWeight: "600",
+		fontSize: 16,
+		color: "#fff",
+	},
+	inactiveFilter: {
+		borderWidth: 1,
+		borderColor: "#75AB19",
+		paddingHorizontal: 15,
+		paddingVertical: 5,
+		borderRadius: 13,
+	},
+	inactiveFilterText: {
+		fontWeight: "600",
+		fontSize: 16,
+		color: "#75AB19",
+	},
+});

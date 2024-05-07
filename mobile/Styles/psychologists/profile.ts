b4965465f90e3fd91d7bd80bb057ec7adc169f55
AliAbdelAall <3laych@gmail.com>
2024-05-08 00:05:31 +0300

    mobile: Styled PsychologistProfile component

---
+++ mobile/Styles/psychologists/profile.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const psychoProfileStyles = StyleSheet.create({
+	profilePicContainer: {
+		backgroundColor: "#75AB19",
+		height: 250,
+		display: "flex",
+		alignItems: "center",
+		paddingTop: 5,
+		borderBottomRightRadius: 30,
+		borderBottomLeftRadius: 30,
+	},
+	profileImage: {
+		width: 200,
+		height: 200,
+		borderRadius: 10,
+	},
+	profileBodyWrapper: {
+		marginTop: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+	},
+});

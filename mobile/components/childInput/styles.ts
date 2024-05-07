32e0cf62e0b0e9ad14681afb1b7927698da4c173
AliAbdelAall <3laych@gmail.com>
2024-05-07 03:02:39 +0300

    mobile: styled ChildInput component

---
+++ mobile/components/childInput/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	inputWrapper: {
+		height: 60,
+		padding: 10,
+		borderColor: "#B3B9CA",
+		borderWidth: 1,
+		borderRadius: 10,
+	},
+	inputlabel: {
+		color: "#75AB19",
+		fontSize: 12,
+	},
+	inputValue: {
+		fontSize: 18,
+		color: "#677294",
+	},
+	placeholder: {
+		color: "#B3B9CA",
+	},
+});

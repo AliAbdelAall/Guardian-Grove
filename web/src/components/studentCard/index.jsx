fa72efb7331489262c798dec092cf2a304bb62d2
AliAbdelAall <3laych@gmail.com>
2024-05-08 21:55:58 +0300

    web: added school info in StudentCard component

--- web/src/components/studentCard/index.jsx
+++ web/src/components/studentCard/index.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 
 // Styles
-import "./style.css"
+import "./style.css";
 
-const StudentCard = ({profilePic, name, age }) => {
-  return (
-    <div className='flex column center student-card'>
-      <img className='student-image' src={profilePic} width={90}  height={90}  alt="profile" />
-      <div className='flex column full-width student-info-wrapper'>
-        <div className='flex space-between full-width student-info'>
-          <p className='font-medium text-acient'>Name</p>
-          <p className='font-light text-acient'>{name}</p>
-        </div>
-        <div className='flex space-between full-width student-info'>
-          <p className='font-medium text-acient'>Age</p>
-          <p className='font-light text-acient'>{`${age} years old`}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+const StudentCard = ({ id, profilePic, name, age, school = "" }) => {
+	return (
+		<div className="flex column center student-card">
+			<img
+				className="student-image"
+				src={profilePic}
+				width={90}
+				height={90}
+				alt="profile"
+			/>
+			<div className="flex column full-width student-info-wrapper">
+				<div className="flex space-between full-width student-info">
+					<p className="font-medium text-acient">Name</p>
+					<p className="font-light text-acient">{name}</p>
+				</div>
+				<div className="flex space-between full-width student-info">
+					<p className="font-medium text-acient">Age</p>
+					<p className="font-light text-acient">{`${age} years old`}</p>
+				</div>
+				{school && (
+					<div className="flex space-between full-width student-info">
+						<p className="font-medium text-acient">school</p>
+						<p className="font-light text-acient">{school}</p>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
 
-export default StudentCard
+export default StudentCard;

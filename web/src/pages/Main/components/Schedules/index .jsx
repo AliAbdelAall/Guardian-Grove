acf2e1749ce3c0e942c2e20f23915f7b5101ae2e
AliAbdelAall <3laych@gmail.com>
2024-05-11 17:36:28 +0300

    web: added handleAddSlotSelection in Schedules component

--- web/src/pages/Main/components/Schedules/index .jsx
+++ web/src/pages/Main/components/Schedules/index .jsx
@@ -7,6 +7,7 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import enUS from "date-fns/locale/en-US";
+import SmallButton from "../../../../components/SmallButton";
 
 const Schedules = () => {
 	const locales = {
@@ -27,33 +28,45 @@ const Schedules = () => {
 			start: new Date(2024, 4, 10, 10, 0),
 			end: new Date(2024, 4, 10, 12, 0),
 			title: "Meeting with Client A",
-			color: "#FF5733",
 		},
 		{
 			id: 2,
 			start: new Date(2024, 4, 12, 14, 0),
 			end: new Date(2024, 4, 12, 16, 0),
 			title: "Presentation at Conference",
-			color: "#33FFA1",
 		},
 	];
 
 	const [events, setEvents] = useState(initialEvents);
-	const [meetingDuration, setMeetingDuration] = useState(60); // Default meeting duration in minutes
+	const [meetingDuration, setMeetingDuration] = useState(30);
 
-	const handleSelectSlot = ({ start }) => {
-		const end = new Date(start);
-		end.setMinutes(end.getMinutes() + meetingDuration);
+	const handleAddSlotSelection = ({ start, end }) => {
+		const startTimestamp = start.getTime();
+		const endTimestamp = end.getTime();
+		const durationInMinutes = (endTimestamp - startTimestamp) / (1000 * 60);
 
-		const newEvent = {
-			id: events.length + 1,
-			start: start,
-			end: end,
-			title: "Available Slot",
-			color: "#33FFA1",
-		};
+		if (durationInMinutes < meetingDuration) {
+			console.log("The selected time range is too small");
+			return;
+		}
 
-		setEvents([...events, newEvent]);
+		const numSlots = Math.floor(durationInMinutes / meetingDuration);
+
+		const newEvents = [];
+		let currentStartTime = startTimestamp;
+		for (let i = 0; i < numSlots; i++) {
+			const currentEndTime =
+				currentStartTime + meetingDuration * 60 * 1000;
+			newEvents.push({
+				id: events.length + i + 1,
+				start: new Date(currentStartTime),
+				end: new Date(currentEndTime),
+				title: "Available Slot",
+			});
+			currentStartTime = currentEndTime;
+		}
+
+		setEvents([...events, ...newEvents]);
 	};
 
 	const handleEventDelete = (eventId) => {
@@ -61,33 +74,46 @@ const Schedules = () => {
 		setEvents(updatedEvents);
 	};
 
+	const eventStyleGetter = (event) => {
+		let backgroundColor;
+		if (event.title === "Available Slot") {
+			backgroundColor = "#677294";
+		} else {
+			backgroundColor = "#75AB19";
+		}
+		return { style: { backgroundColor } };
+	};
+
 	return (
-		<div className="full-width calendar-container">
-			<div>
-				<label htmlFor="meetingDuration">
-					Meeting Duration (minutes)
-				</label>
-				<input
-					type="number"
-					id="meetingDuration"
-					value={meetingDuration}
-					onChange={(e) =>
-						setMeetingDuration(parseInt(e.target.value))
-					}
+		<div className="full-width">
+			<div className="full-width calendar-container">
+				<div>
+					<label htmlFor="meetingDuration">
+						Meeting Duration (minutes)
+					</label>
+					<input
+						type="number"
+						id="meetingDuration"
+						value={meetingDuration}
+						onChange={(e) =>
+							setMeetingDuration(parseInt(e.target.value))
+						}
+					/>
+				</div>
+				<Calendar
+					selectable
+					localizer={localizer}
+					events={events}
+					startAccessor="start"
+					endAccessor="end"
+					onSelectSlot={handleAddSlotSelection}
+					onSelectEvent={(event) => handleEventDelete(event.id)}
+					style={{ height: 700 }}
+					popup={true}
+					popupOffset={50}
+					eventPropGetter={eventStyleGetter}
 				/>
 			</div>
-			<Calendar
-				selectable
-				localizer={localizer}
-				events={events}
-				startAccessor="start"
-				endAccessor="end"
-				onSelectSlot={handleSelectSlot}
-				onSelectEvent={(event) => handleEventDelete(event.id)}
-				style={{ height: 750, width: 1150 }}
-				popup={true}
-				popupOffset={50}
-			/>
 		</div>
 	);
 };

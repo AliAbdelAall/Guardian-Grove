7d0515575e40a076664e1fa0e8d3fe6919288d2d
AliAbdelAall <3laych@gmail.com>
2024-05-12 21:59:13 +0300

    backend: added socket.io configuratiion

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import rootRouter from "./Routes/root.routes";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 const app: Express = express();
 
@@ -11,7 +13,6 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", rootRouter);
 app.use(express.static("public"));
 
@@ -19,6 +20,33 @@ export const prismaClient = new PrismaClient({
 	log: ["query"],
 });
 
-app.listen(port, () => {
+const server = http.createServer(app);
+if (
+	!process.env.SOCKET_WEB_SERVER_URL ||
+	!process.env.SOCKET_MOBILE_SERVER_URL
+) {
+	console.log("ENV NOT CONNECTED!");
+} else {
+	console.log(process.env.SOCKET_WEB_SERVER_URL);
+	console.log(process.env.SOCKET_MOBILE_SERVER_URL);
+}
+const io = new Server(server, {
+	cors: {
+		origin: [
+			process.env.SOCKET_WEB_SERVER_URL!,
+			process.env.SOCKET_MOBILE_SERVER_URL!,
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log("User Connected: ", socket.id);
+	socket.on("disconnect", () => {
+		console.log("User disconnected: ", socket.id);
+	});
+});
+
+server.listen(port, () => {
 	console.log(`app is working on port ${port}`);
 });

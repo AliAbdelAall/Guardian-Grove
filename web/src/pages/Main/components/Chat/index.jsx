985e87d2c378666aa852c1a948ff009f8d770a6e
AliAbdelAall <3laych@gmail.com>
2024-05-13 00:22:33 +0300

    web: updated Chat component

--- web/src/pages/Main/components/Chat/index.jsx
+++ web/src/pages/Main/components/Chat/index.jsx
@@ -7,10 +7,10 @@ const Chat = () => {
 	return (
 		<div className="flex chat-container">
 			<div className="flex column consesations-search-wrapper">
-				<input type="text" />
+				<input type="text" placeholder="Search" />
 
 				<div className="flex column conversations-wrapper">
-					<div className="flex space between conversation">
+					<div className="flex space-between conversation">
 						<div className="flex center conversation-info">
 							<img src={ProfilePic} alt="image" />
 							<p>name name</p>
@@ -18,7 +18,7 @@ const Chat = () => {
 						<div className="dot-marker"></div>
 					</div>
 
-					<div className="conversation">
+					<div className="flex space-between conversation">
 						<div className="flex center conversation-info">
 							<img src={ProfilePic} alt="image" />
 							<p>name name</p>
@@ -27,23 +27,34 @@ const Chat = () => {
 					</div>
 				</div>
 			</div>
-			<div className="messages-container">
-				<div className="messages header"></div>
+			<div className="flex column messages-container">
+				<div className="messages-header"></div>
 
-				<div className="messages-wrapper">
-					<p>date</p>
-					<div className="recieved-message-wrapper">
-						<p className="recieved-message">message</p>
-						<p className="message time">3:00 pm</p>
+				<div className="flex column messages-wrapper">
+					<p className="messages-date">2024-04-23</p>
+
+					<div className="flex column recieved-message-wrapper">
+						<p className="recieved-message">
+							hello i just want to check if you can take care of
+							my child reports ?
+						</p>
+						<p className="message-time">3:00 pm</p>
 					</div>
-					<div className="sent-message-wrapper">
-						<p className="sent-message">message</p>
-						<p className="message time">3:00 pm</p>
+					<div className="flex column sent-message-wrapper">
+						<p className="sent-message">
+							sure what is you son name so i can start sending
+							reports starting tomoorw
+						</p>
+						<p className="message-time">3:00 pm</p>
 					</div>
 				</div>
-				<div>
-					<input type="text" />
-					<button>send</button>
+				<div className="flex message-input-wrapper">
+					<textarea
+						className="message-input"
+						placeholder="Message..."
+						type="text"
+					/>
+					<button className="message-send-btn">Send</button>
 				</div>
 			</div>
 		</div>

e16773050198ae5503bad4595174c0da7847a424
AliAbdelAall <3laych@gmail.com>
2024-05-14 04:14:41 +0300

    mobile: styled Chat component

---
+++ mobile/Styles/chatStyles.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+export const chatStyles = StyleSheet.create({
+	chatContainer: {
+		padding: 20,
+	},
+	conversationImage: {
+		height: 70,
+		width: 70,
+		borderRadius: 35,
+	},
+	conversationWrapper: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		padding: 5,
+		borderRadius: 10,
+		backgroundColor: "#ECECEC",
+		marginBottom: 10,
+	},
+	conversationInfoWrapper: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 10,
+	},
+	conversationName: {
+		fontSize: 20,
+		color: "#677294",
+	},
+	onlineDot: {
+		width: 15,
+		height: 15,
+		backgroundColor: "#75AB19",
+		borderRadius: 8,
+	},
+});

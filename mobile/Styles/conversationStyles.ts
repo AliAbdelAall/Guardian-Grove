7202c54c69ee3868c2abfb73fc16edf21246666c
AliAbdelAall <3laych@gmail.com>
2024-05-15 00:56:44 +0300

    mobile: styled Conversation component

---
+++ mobile/Styles/conversationStyles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+export const conversationStyles = StyleSheet.create({
+	dayTextStyle: {
+		color: "#677294",
+		fontSize: 16,
+	},
+	bubbleText: {
+		color: "#fff",
+		fontSize: 18,
+	},
+	bubbleWrapperRight: {
+		backgroundColor: "#75AB19",
+	},
+	bubbleWrapperLeft: {
+		backgroundColor: "#B3B9CA",
+	},
+	bubbleTimeText: {
+		color: "#fff",
+		fontSize: 12,
+	},
+	sendButton: {
+		marginRight: 16,
+		marginBottom: 8,
+	},
+	sendButtonText: {
+		color: "#fff",
+		fontWeight: "500",
+		fontSize: 16,
+	},
+});

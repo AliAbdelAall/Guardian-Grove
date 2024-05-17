051382bc6d4164f81977a7c6060d260d079bf1f0
AliAbdelAall <3laych@gmail.com>
2024-05-17 15:02:38 +0300

    backend: added DeviceToken model and migration

---
+++ backend/prisma/migrations/20240517115949_creacte_devicetoken_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `DeviceToken` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `DeviceToken_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `DeviceToken` ADD CONSTRAINT `DeviceToken_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

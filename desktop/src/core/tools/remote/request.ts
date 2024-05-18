ccd1e9fe317d61f4f6051af6aead69ab0639b9cd
AliAbdelAall <3laych@gmail.com>
2024-05-18 18:12:15 +0300

    desktop: added custom axios hook

---
+++ desktop/src/core/tools/remote/request.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios";
+import { getLocalUser, removeLocalUser } from "../local/user";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = "http://localhost:3000";
+
+export const useSendRequest = () => {
+	const token = getLocalUser() ?? "";
+	const navigate = useNavigate();
+
+	const sendRequest = async (method: string, route: string, body: any) => {
+		const headers = {
+			Authorization: `bearer ${token}`,
+			"Content-Type":
+				body instanceof FormData
+					? "multipart/form-data"
+					: "application/json",
+		};
+		try {
+			const response = await axios.request({
+				method,
+				url: route,
+				data: body,
+				headers,
+			});
+			return response;
+		} catch (error) {
+			const status = (error as AxiosError).response?.status;
+			if (status === 401) {
+				removeLocalUser();
+				navigate("/");
+			}
+			console.error(error);
+			throw error;
+		}
+	};
+	return sendRequest;
+};

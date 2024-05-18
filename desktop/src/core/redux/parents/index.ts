5f60785944093b338faa4f3f258f635f6bc21c32
AliAbdelAall <3laych@gmail.com>
2024-05-18 23:38:23 +0300

    desktop: added redux parentsSlice

---
+++ desktop/src/core/redux/parents/index.ts
@@ -0,0 +1,24 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface parent {
+	id: number;
+	name: string;
+	email: string;
+	children: string[];
+}
+
+const initialState: parent[] = [];
+
+const parentsSlice = createSlice({
+	initialState,
+	name: "parentsSlice",
+	reducers: {
+		setParents: (state, action: PayloadAction<parent[]>) => {
+			return action.payload;
+		},
+	},
+});
+
+export const { setParents } = parentsSlice.actions;
+export const parentsSliceName = parentsSlice.name;
+export default parentsSlice.reducer;
--- desktop/src/core/redux/store.ts
+++ desktop/src/core/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 // Logger
 import logger from "redux-logger";
+import parentsReducer, { parentsSliceName } from "./parents";
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		[parentsSliceName]: parentsReducer,
+	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 export type RootState = ReturnType<typeof store.getState>;

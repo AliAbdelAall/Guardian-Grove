94c41a691ad2929cf22a741097b9f8fbbd8ac3cd
AliAbdelAall <3laych@gmail.com>
2024-05-18 23:41:26 +0300

    desktop: added redux teachersSlice

--- desktop/src/core/redux/store.ts
+++ desktop/src/core/redux/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-// Logger
-import logger from "redux-logger";
+
+// Slices
 import parentsReducer, { parentsSliceName } from "./parents";
 import psychologistsReducer, { psycologistsSliceName } from "./psychologists";
+import teachersReducer, { teachersSliceName } from "./teachers";
+
+// Logger
+import logger from "redux-logger";
+
 export const store = configureStore({
 	reducer: {
 		[parentsSliceName]: parentsReducer,
 		[psycologistsSliceName]: psychologistsReducer,
+		[teachersSliceName]: teachersReducer,
 	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
---
+++ desktop/src/core/redux/teachers/index.ts
@@ -0,0 +1,24 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface teacher {
+	id: number;
+	name: string;
+	email: string;
+	teachers: string[];
+}
+
+const initialState: teacher[] = [];
+
+const teachersSlice = createSlice({
+	initialState,
+	name: "teachersSlice",
+	reducers: {
+		setTeachers: (state, action: PayloadAction<teacher[]>) => {
+			return action.payload;
+		},
+	},
+});
+
+export const { setTeachers } = teachersSlice.actions;
+export const teachersSliceName = teachersSlice.name;
+export default teachersSlice.reducer;

e31b51927aa5acf65cb5593f343942ae8d20d2ea
AliAbdelAall <3laych@gmail.com>
2024-05-18 17:42:49 +0300

    desktop: added routes in App component

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,5 +1,38 @@
-function App() {
-	return <div>App</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Styles
+import "./styles/main.css";
+import "./styles/utilities.css";
+import "./styles/colors.css";
+
+// Taostify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Layouts
+import Auth from "./pages/Auth";
+const App = () => {
+	return (
+		<BrowserRouter>
+			<ToastContainer
+				position="top-right"
+				autoClose={2500}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
+			<Routes>
+				<Route path="/" element={<Auth />}>
+          
+        </Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;

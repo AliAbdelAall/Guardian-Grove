11eec90bc1a603e86902f25e402940e1030e4a5e
AliAbdelAall <3laych@gmail.com>
2024-05-19 14:49:35 +0300

    desktop: added handleDeleteParent function in Parents component

--- desktop/src/pages/Main/components/Parents/index.tsx
+++ desktop/src/pages/Main/components/Parents/index.tsx
@@ -1,16 +1,45 @@
-import { FC, useState } from "react";
-import { useSelector } from "react-redux";
+import { FC, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../core/redux/store";
 import DeleteButton from "../../../../components/DelelteButton";
+import { useSendRequest } from "../../../../core/tools/remote/request";
+import { requestMethods } from "../../../../core/enums/requestMethods";
+import { toast } from "react-toastify";
+import { deleteParent } from "../../../../core/redux/parents";
 
 const Parents: FC = () => {
 	const { parents } = useSelector((global: RootState) => global.parentsSlice);
 
-	const [filterdParents, setFilterdParents] = useState(parents);
+	const [filteredParents, setFilteredParents] = useState(parents);
 
+	const sendRequest = useSendRequest();
+	const dispatch = useDispatch();
+
+	const [deletedId, setDeletedId] = useState<number>(0);
+	console.log(deletedId);
+
+	useEffect(() => {
+		setFilteredParents(parents);
+	}, [parents]);
+
+	const handleDeleteParent = (id: number) => {
+		sendRequest(requestMethods.POST, "api/admin/delete-user", {
+			userId: id,
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					dispatch(deleteParent(id));
+					setDeletedId(id);
+				}
+			})
+			.catch((error: any) => {
+				console.log(error);
+				toast.error(error.response.error);
+			});
+	};
 	const handleParentsSearch = (value: string) => {
 		const userSearch = value.toLowerCase();
-		setFilterdParents(
+		setFilteredParents(
 			parents.filter(
 				(parent) =>
 					parent.name.toLowerCase().includes(userSearch) ||
@@ -30,7 +59,7 @@ const Parents: FC = () => {
 					onChange={(e: any) => handleParentsSearch(e.target.value)}
 				/>
 			</div>
-			{filterdParents.length !== 0 ? (
+			{filteredParents.length !== 0 ? (
 				<table className="table full-width">
 					<thead>
 						<tr className="text-acient">
@@ -42,7 +71,7 @@ const Parents: FC = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{filterdParents?.map((parent) => {
+						{filteredParents?.map((parent) => {
 							const { id, name, email, children, profilePic } =
 								parent;
 							return (
@@ -63,7 +92,11 @@ const Parents: FC = () => {
 										{children.join(", ")}
 									</td>
 									<td className="tr-end">
-										<DeleteButton handleClick={() => {}} />
+										<DeleteButton
+											handleClick={() =>
+												handleDeleteParent(id)
+											}
+										/>
 									</td>
 								</tr>
 							);

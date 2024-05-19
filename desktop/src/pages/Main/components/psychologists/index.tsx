03e56b85d13403ab9924cf19713fef2e36b8bf11
AliAbdelAall <3laych@gmail.com>
2024-05-19 14:50:23 +0300

    desktop: added handleDeletePsychologist function in Psychologists component

--- desktop/src/pages/Main/components/psychologists/index.tsx
+++ desktop/src/pages/Main/components/psychologists/index.tsx
@@ -1,19 +1,50 @@
-import { FC, useState } from "react";
-import { useSelector } from "react-redux";
+import { FC, useEffect, useState } from "react";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../core/redux/store";
 import DeleteButton from "../../../../components/DelelteButton";
+import { useSendRequest } from "../../../../core/tools/remote/request";
+import { requestMethods } from "../../../../core/enums/requestMethods";
+import { toast } from "react-toastify";
+import { deletePsychologist } from "../../../../core/redux/psychologists";
 
 const Psychologists: FC = () => {
 	const { psychologists } = useSelector(
 		(global: RootState) => global.psychologistsSlice
 	);
+	const sendRequest = useSendRequest();
+	const dispatch = useDispatch();
 
-	const [filterdPsychologists, setFilterdPsychologists] =
+	const [filteredPsychologists, setFilteredPsychologists] =
 		useState(psychologists);
+
+	useEffect(() => {
+		setFilteredPsychologists(psychologists);
+	}, [Psychologists]);
+
+	const [deletedId, setDeletedId] = useState<number>(0);
+	console.log(deletedId);
+
+	const handleDeletePsychologist = (id: number) => {
+		sendRequest(requestMethods.POST, "api/admin/delete-user", {
+			userId: id,
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					dispatch(deletePsychologist(id));
+					setDeletedId(id);
+				}
+			})
+			.catch((error: any) => {
+				console.log(error);
+				toast.error(error.response.error);
+			});
+	};
 
 	const handlePsychologistsSearch = (value: string) => {
 		const userSearch = value.toLowerCase();
-		setFilterdPsychologists(
+		setFilteredPsychologists(
 			psychologists.filter(
 				(psychologist) =>
 					psychologist.name.toLowerCase().includes(userSearch) ||
@@ -36,7 +67,7 @@ const Psychologists: FC = () => {
 					}
 				/>
 			</div>
-			{filterdPsychologists.length !== 0 ? (
+			{filteredPsychologists.length !== 0 ? (
 				<table className="table full-width">
 					<thead>
 						<tr className="text-acient">
@@ -49,7 +80,7 @@ const Psychologists: FC = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{filterdPsychologists?.map((psychologist) => {
+						{filteredPsychologists?.map((psychologist) => {
 							const {
 								id,
 								name,
@@ -75,7 +106,11 @@ const Psychologists: FC = () => {
 									<td className="tr-middle">{speciality}</td>
 									<td className="tr-middle">{rating}</td>
 									<td className="tr-end">
-										<DeleteButton handleClick={() => {}} />
+										<DeleteButton
+											handleClick={() =>
+												handleDeletePsychologist(id)
+											}
+										/>
 									</td>
 								</tr>
 							);

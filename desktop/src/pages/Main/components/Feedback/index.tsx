6e3ee0eb9182bea24163343d4564d5e9b7955e6b
AliAbdelAall <3laych@gmail.com>
2024-05-19 16:14:19 +0300

    desktop: added handleDeleteReview reducer in Reviews component

--- desktop/src/pages/Main/components/Feedback/index.tsx
+++ desktop/src/pages/Main/components/Feedback/index.tsx
@@ -59,6 +59,22 @@ const Feedback: FC = () => {
 			});
 	};
 
+	const handleDeleteReview = (id: number) => {
+		sendRequest(requestMethods.POST, "api/admin/delete-review", {
+			reviewId: id,
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					dispatch(deleteReview(id));
+					setDeletedId(id);
+				}
+			})
+			.catch((error: any) => {
+				console.log(error);
+				toast.error(error.response.error);
+			});
+	};
+
 	const handleReviewsSearch = (value: string) => {
 		const userSearch = value.toLowerCase();
 		setFilteredReviews(

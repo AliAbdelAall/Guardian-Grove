7362089819c510ed4085baa14cd5a1479b4f76b6
AliAbdelAall <3laych@gmail.com>
2024-05-19 13:07:58 +0300

    desktop: added handleParentsSearch function in Parents component

--- desktop/src/pages/Main/components/Parents/index.tsx
+++ desktop/src/pages/Main/components/Parents/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../core/redux/store";
 import DeleteButton from "../../../../components/DelelteButton";
@@ -6,10 +6,31 @@ import DeleteButton from "../../../../components/DelelteButton";
 const Parents: FC = () => {
 	const { parents } = useSelector((global: RootState) => global.parentsSlice);
 
+	const [filterdParents, setFilterdParents] = useState(parents);
+
+	const handleParentsSearch = (value: string) => {
+		const userSearch = value.toLowerCase();
+		setFilterdParents(
+			parents.filter(
+				(parent) =>
+					parent.name.toLowerCase().includes(userSearch) ||
+					parent.email.toLowerCase().includes(userSearch)
+			)
+		);
+	};
+
 	return (
 		<div className="feedback-container full-width">
 			<h2 className="text-acient page-header">Parents</h2>
-			{parents.length !== 0 ? (
+			<div>
+				<input
+					className="search-input "
+					placeholder="Search"
+					type="text"
+					onChange={(e: any) => handleParentsSearch(e.target.value)}
+				/>
+			</div>
+			{filterdParents.length !== 0 ? (
 				<table className="table full-width">
 					<thead>
 						<tr className="text-acient">
@@ -21,7 +42,7 @@ const Parents: FC = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{parents?.map((parent) => {
+						{filterdParents?.map((parent) => {
 							const { id, name, email, children, profilePic } =
 								parent;
 							return (
@@ -51,7 +72,11 @@ const Parents: FC = () => {
 				</table>
 			) : (
 				<div className="flex full-width center">
-					<h3>You have no Parents yet.</h3>
+					<h3>{`${
+						parents
+							? "No parents with filter"
+							: "You have no parents yet."
+					}`}</h3>
 				</div>
 			)}
 		</div>
